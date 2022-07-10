a660ef9189f8249965a4b4552ec7eddee5c077c9
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2022-07-10 20:44:22 +0800

    Level

---
+++ network.js
@@ -0,0 +1,41 @@
+class Level {
+  constructor(inputCount, outputCount) {
+    this.inputs = new Array(inputCount);
+    this.outputs = new Array(outputCount);
+    this.biases = new Array(outputCount);
+
+    this.weights = [];
+    for (let i = 0; i < inputCount; i++) {
+      this.weights[i] = new Array(outputCount);
+    }
+
+    Level.#randomize(this);
+  }
+
+  static #randomize(level) {
+    for (let i = 0; i < level.inputs.length; i++) {
+      for (let j = 0; j < level.outputs.length; j++) {
+        level.weights[i][j] = Math.random() * 2 - 1;
+      }
+    }
+
+    for (let i = 0; level.biases.length; i++) {
+      level.biases[i] = Math.random() * 2 - 1;
+    }
+  }
+
+  static feedForward(givenInputs, level) {
+    for (let i = 0; i < level.inputs.length; i++) {
+      level.inputs[i] = givenInputs[i];
+    }
+
+    for (let i = 0; i < level.outputs.length; i++) {
+      const sum = level.inputs
+        .map(input, (index) => input * level.weights[index][i])
+        .reduce((a, b) => a + b, 0);
+      level.outputs[i] = sum + level.biases[i] > 0 ? 1 : 0;
+    }
+
+    return level.outputs;
+  }
+}
