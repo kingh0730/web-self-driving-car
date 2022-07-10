d30f4c9c7fcbae038365ca851e97f4f957aa06e7
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2022-07-10 19:01:14 +0800

    fix bug

--- main.js
+++ main.js
@@ -8,12 +8,13 @@ const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 animate();
 
 function animate() {
+  car.update(road.borders);
+
   canvas.height = window.innerHeight;
 
   ctx.save();
   ctx.translate(0, -car.y + canvas.height * 0.8);
   road.draw(ctx);
-  car.update(road.borders);
   car.draw(ctx);
 
   ctx.restore();
