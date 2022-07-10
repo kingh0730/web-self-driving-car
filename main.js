1db04218fd3feadb47c94ca60b3eb90028f3bd52
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2022-07-10 22:37:02 +0800

    cars

--- main.js
+++ main.js
@@ -7,27 +7,43 @@ const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI", 10);
+
+const myCar = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS", 10);
+const cars = generateCars(100);
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 8)];
 
 animate();
 
+function generateCars(N) {
+  const cars = [];
+  for (let i = 0; i < N; i++) {
+    cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI", 10));
+  }
+  return cars;
+}
+
 function animate(time) {
   traffic.forEach((c) => c.update(road.borders, []));
-  car.update(road.borders, traffic);
+  cars.forEach((c) => c.update(road.borders, traffic));
+  myCar.update(road.borders, traffic);
 
   carCanvas.height = window.innerHeight;
   networkCanvas.height = window.innerHeight;
 
   carCtx.save();
-  carCtx.translate(0, -car.y + carCanvas.height * 0.8);
+  carCtx.translate(0, -myCar.y + carCanvas.height * 0.8);
   road.draw(carCtx);
   traffic.forEach((c) => c.draw(carCtx, "red"));
-  car.draw(carCtx, "blue");
+
+  carCtx.globalAlpha = 0.2;
+  cars.forEach((c) => c.draw(carCtx, "blue"));
+  carCtx.globalAlpha = 1;
+
+  myCar.draw(carCtx, "blue");
 
   carCtx.restore();
 
   networkCtx.lineDashOffset = -time / 50;
-  Visualizer.drawNetwork(networkCtx, car.brain);
+  Visualizer.drawNetwork(networkCtx, myCar.brain);
   requestAnimationFrame(animate);
 }
