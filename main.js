4730bcf73b1c5d4614ec4f6f58cdac458ee24d91
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2022-07-10 21:25:48 +0800

    visualizer

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const carCanvas = document.getElementById("carCanvas");
 carCanvas.width = 200;
+const networkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width = 300;
 
 const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI", 10);
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 8)];
@@ -13,6 +17,7 @@ function animate() {
   car.update(road.borders, traffic);
 
   carCanvas.height = window.innerHeight;
+  networkCanvas.height = window.innerHeight;
 
   carCtx.save();
   carCtx.translate(0, -car.y + carCanvas.height * 0.8);
@@ -21,5 +26,7 @@ function animate() {
   car.draw(carCtx, "blue");
 
   carCtx.restore();
+
+  Visualizer.drawNetwork(networkCtx, car.brain);
   requestAnimationFrame(animate);
 }
