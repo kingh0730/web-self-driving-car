92f73e373f5fb967c00150066f3a97f8bfb8fcc2
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2022-07-10 21:16:39 +0800

    networkCanvas

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const canvas = document.getElementById("carCanvas");
-canvas.width = 200;
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 200;
 
-const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const ctx = carCanvas.getContext("2d");
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI", 10);
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 8)];
 
@@ -12,10 +12,10 @@ function animate() {
   traffic.forEach((c) => c.update(road.borders, []));
   car.update(road.borders, traffic);
 
-  canvas.height = window.innerHeight;
+  carCanvas.height = window.innerHeight;
 
   ctx.save();
-  ctx.translate(0, -car.y + canvas.height * 0.8);
+  ctx.translate(0, -car.y + carCanvas.height * 0.8);
   road.draw(ctx);
   traffic.forEach((c) => c.draw(ctx, "red"));
   car.draw(ctx, "blue");
