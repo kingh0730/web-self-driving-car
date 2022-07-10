f57b1e47e5fc1ced138e87354be56ba0b7e21a04
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2022-07-10 17:39:21 +0800

    road

---
+++ road.js
@@ -0,0 +1,33 @@
+class Road {
+  constructor(x, width, laneCount = 3) {
+    this.x = x;
+    this.width = width;
+    this.laneCount = laneCount;
+
+    this.left = x - width / 2;
+    this.right = x + width / 2;
+
+    const infinity = 1000000;
+    this.top = -infinity;
+    this.bottom = infinity;
+  }
+
+  getLaneCenter(laneIndex) {
+    const laneWidth = this.width / this.laneCount;
+    return this.left + laneWidth / 2 + laneIndex * laneWidth;
+  }
+
+  draw(ctx) {
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "white";
+
+    for (let i = 0; i <= this.laneCount; i++) {
+      const x = lerp(this.left, this.right, i / this.laneCount);
+      ctx.setLineDash(i > 0 && i < this.laneCount ? [20, 20] : []);
+      ctx.beginPath();
+      ctx.moveTo(x, this.top);
+      ctx.lineTo(x, this.bottom);
+      ctx.stroke();
+    }
+  }
+}
