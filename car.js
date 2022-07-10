bebed2b981dd0bf78b7b05e689b0dccd15047604
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2022-07-10 18:11:02 +0800

    sensor

--- car.js
+++ car.js
@@ -12,11 +12,13 @@ class Car {
 
     this.angle = 0;
 
+    this.sensor = new Sensor(this);
     this.controls = new Controls();
   }
 
   update() {
     this.#move();
+    this.sensor.update();
   }
 
   #move() {
@@ -68,5 +70,7 @@ class Car {
     ctx.fill();
 
     ctx.restore();
+
+    this.sensor.draw(ctx);
   }
 }
