6c710e1a7ded121ac9158422f22bfc89d3637a4d
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2022-07-10 19:52:14 +0800

    traffic

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car {
-  constructor(x, y, width, height) {
+  constructor(x, y, width, height, controlType, maxSpeed) {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -8,13 +8,13 @@ class Car {
 
     this.speed = 0;
     this.acceleration = 0.2;
-    this.maxSpeed = 10;
+    this.maxSpeed = maxSpeed;
     this.friction = 0.05;
 
     this.damaged = false;
 
     this.sensor = new Sensor(this);
-    this.controls = new Controls();
+    this.controls = new Controls(controlType);
   }
 
   update(roadBorders) {
--- main.js
+++ main.js
@@ -3,11 +3,13 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS", 10);
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 8)];
 
 animate();
 
 function animate() {
+  traffic.forEach((c) => c.update(road.borders));
   car.update(road.borders);
 
   canvas.height = window.innerHeight;
@@ -15,6 +17,7 @@ function animate() {
   ctx.save();
   ctx.translate(0, -car.y + canvas.height * 0.8);
   road.draw(ctx);
+  traffic.forEach((c) => c.draw(ctx));
   car.draw(ctx);
 
   ctx.restore();
