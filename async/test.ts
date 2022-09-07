c419a208d13afa37327bb8b8cfbf2c1ea0d83e59
kingh0730 <kingh0730@berkeley.edu>
2022-09-07 16:27:53 +0800

    async

---
+++ async/test.ts
@@ -0,0 +1,42 @@
+const setTimeoutAsync = (cb, delay) => new Promise((resolve) => {
+    setTimeout(() => {
+        resolve(cb());
+    }, delay);
+});
+function printSync() {
+    console.log("sync");
+}
+async function printAsync(time) {
+    await setTimeoutAsync(() => {
+        time > 0 ? console.log("async") : null;
+    }, time)
+}
+
+const arr: Array<any> = []
+async function produce() {
+    for (let i = 0; i < 10; i++) {
+        console.log(`produing ${i}`);
+        await printAsync(1000);
+        arr.push(i);
+    }
+    arr.push(null)
+}
+async function consume() {
+    while (true) {
+        const item = arr.length ? arr.shift() : undefined;
+        if (item === null) {
+            break;
+        } else if (item === undefined) {
+            await printAsync(0);
+            continue;
+        }
+        await printAsync(10);
+        console.log(`consumed ${item}`);
+    }
+}
+
+async function main() {
+    await Promise.all([produce(), consume()])
+    console.log("done")
+}
+main()
