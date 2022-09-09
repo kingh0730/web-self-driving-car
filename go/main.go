38ff55f9af5d7b9ba2e54f787f7672ca470f47fb
kingh0730 <kingh0730@berkeley.edu>
2022-09-09 15:19:35 +0800

    matrix

--- go/main.go
+++ go/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	matrix := [...][3]int{
+		[...]int{1, 2, 3},
+		[...]int{1, 2, 3},
+		[...]int{1, 2, 3},
+	}
+	fmt.Println(matrix)
 }
